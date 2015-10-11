8c20ade28d83d6764e352d8b658bb08fb12c2d98
Vasanth Pappu <vpappu@covermymeds.com>
2015-10-11 10:30:55 -0400

    character counting v2, with message passing

--- ch2/2-character-counting.exs
+++ ch2/2-character-counting.exs
@@ -1,17 +1,43 @@
 defmodule Exercise2 do
-  def prompt do
-    IO.gets("What is the input string? ") |> String.strip
+  def prompter(sendee) do
+    string = IO.gets("What is the input string? ") |> String.strip
+
+    send(sendee, {self, :string, string})
+
+    receive do
+      {:continue} ->
+    end
+
+    prompter sendee
   end
 
-  def string_count(string) do
-    string |> String.length
+  def string_counter (sendee) do
+    receive do
+      {sender, :string, string} ->
+        send(sendee, {self, :length, string |> String.length, string })
+
+      string -> send(sendee, {:length, string |> String.length })
+    end
+    string_counter sendee
   end
 
-  def state_length(string) do
-    IO.puts "#{string} has #{string_count string} characters"
+  def length_stater do
+    receive do
+      {:length, length} -> IO.puts "some string has #{length} characters"
+      {sender, :length, length, string} ->
+        IO.puts "#{string} has #{length} characters"
+        send(sender, {:continue})
+    end
+
+    length_stater
   end
 
 end
 
 
-Exercise2.prompt |> Exercise2.state_length
+length_stater = spawn(Exercise2, :length_stater, [])
+string_counter = spawn(Exercise2, :string_counter, [length_stater])
+
+Exercise2.prompter string_counter
+
+
